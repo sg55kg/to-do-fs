b7618b6b09dd2becbc9c18494652ce90bd7c23a6
Sam Dollar <sdollar1@gmail.com>
2021-09-23 14:27:21 -0500

    taskform

--- client/src/components/TaskForm/TaskForm.js
+++ client/src/components/TaskForm/TaskForm.js
@@ -7,7 +7,7 @@ import { updateList } from '../../actions/lists';
 const TaskForm = ({ showTaskForm, setShowTaskForm, selectList }) => {
     
 
-    const [taskData, setTaskData] = useState()
+    const [taskData, setTaskData] = useState([name: '', priority: 0, description: '', dueBy: ''])
 
     const dispatch = useDispatch();
 
@@ -30,13 +30,13 @@ const TaskForm = ({ showTaskForm, setShowTaskForm, selectList }) => {
         <form className="task-form">
             <h3>Add a task to this list</h3> {/*get this list name and replace 'list' with {list.title}*/ }
             <label>Task name:</label>
-            <input type="text" onChange={(e) => setTaskData()}></input>
+            <input type="text" onChange={(e) => setTaskData([...taskData, name: e.target.value])}></input>
             <label>Set task priority:</label>
-            <input type="number" onChange={(e) => setTaskData()}></input>
+            <input type="number" onChange={(e) => setTaskData([...taskData, priority: e.target.value])}></input>
             <label>Description:</label>
-            <input type="text" onChange={(e) => setTaskData()}></input>
+            <input type="text" onChange={(e) => setTaskData([...taskData, description: e.target.value])}></input>
             <label>When does this need to be finished?</label>
-            <input type="date" onChange={(e) => setTaskData()}></input>
+            <input type="date" onChange={(e) => setTaskData([...taskData, dueBy: e.target.value])}></input>
             <button className="submit-new-task" onClick={handleSubmit}>Add Task</button>
             <button className="cancel-submit-task" onClick={cancelSubmit}>Cancel</button>
         </form>
